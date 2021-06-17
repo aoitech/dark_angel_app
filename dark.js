e8d8284430a95821522f9205299dd64841e33b15
Aoi Onishi <53684526+aoitech@users.noreply.github.com>
2021-06-17 13:53:30 +0900

    ランダム値表示

--- dark.js
+++ dark.js
@@ -2,13 +2,19 @@ $(function() {
   $("#form-submit").on('click', function() {
     const word = $("#form-text").val();
     if(word.length > 0) {
-      const replace_word = word.replace(word,"hello")
-      console.log(replace_word)
-      $(".header-title").before(replace_word)
+      const messages = ["hoge","fuga","hey"]
+
+      // Math.random=0以上1未満の疑似ランダムな浮動小数点による小数を返すメソッド
+      const messageNum = Math.floor( Math.random() * messages.length);
+      const replace_word = word.replace(word,messages[messageNum])
+      const headerTitleWord = $("#form-text").val(replace_word)
+
+      $("#reset-btn").on('click', function() {
+        headerTitleWord.remove();
+      })
       }
     else if(word == "")  {
       alert("聖なるフォームに好きな文字を入力してね！")
     }
-     
   })
 })
