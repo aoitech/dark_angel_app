b4d5aa5bc95a4be289a66ab1ed407328c3496cab
Aoi Onishi <53684526+aoitech@users.noreply.github.com>
2021-06-17 13:32:26 +0900

    簡易版置換

--- dark.js
+++ dark.js
@@ -2,7 +2,9 @@ $(function() {
   $("#form-submit").on('click', function() {
     const word = $("#form-text").val();
     if(word.length > 0) {
-       console.log("hello!");
+      const replace_word = word.replace(word,"hello")
+      console.log(replace_word)
+      $(".header-title").before(replace_word)
       }
     else if(word == "")  {
       alert("聖なるフォームに好きな文字を入力してね！")
